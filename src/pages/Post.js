b83884f89631930312c56c52e6dfee3217fd9472
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-27 12:40:54 +0000

    Commit

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,38 +1,38 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext } from 'react';
 import BlogContext from '../context/BlogContext';
 import { useParams } from 'react-router-dom';
-import Spinner from '../components/Spinner'
+import Spinner from '../components/Spinner';
 
 const Post = () => {
-    const blogContext = useContext(BlogContext);
-    const {
-        getPostById,
-        currentBlogPost
-    } = blogContext;
+  const blogContext = useContext(BlogContext);
+  const { getPostById, currentBlogPost } = blogContext;
 
-    let { postId } = useParams();
+  let { postId } = useParams();
 
-    useEffect(() => {
-        getPostById(postId);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  useEffect(() => {
+    getPostById(postId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    //console.log(postId)
-    return (
-        <div className='post'>
-            {currentBlogPost ? (
-                <>
-                    <div className='post-image overlay' style={{ backgroundImage: `url('${currentBlogPost.image}')` }}></div>
-                    <div className='post-content'>
-                        <h2 className="title">{currentBlogPost.title}</h2>
-                        <p className="text">{currentBlogPost.content}</p>
-                    </div>
-                </>
-            ) : (
-                    <Spinner />
-                )}
-        </div>
-    )
-}
+  //console.log(postId)
+  return (
+    <div className='post'>
+      {currentBlogPost ? (
+        <>
+          <div
+            className='post-image overlay'
+            style={{ backgroundImage: `url('${currentBlogPost.image}')` }}
+          ></div>
+          <div className='post-content'>
+            <h2 className='title'>{currentBlogPost.title}</h2>
+            <p className='text'>{currentBlogPost.content}</p>
+          </div>
+        </>
+      ) : (
+        <Spinner />
+      )}
+    </div>
+  );
+};
 
 export default Post;
