bffdffb8c8c70b9612db0b5106b0a4f9d242c293
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-27 11:56:05 +0000

    Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <div className="container">
-                <Link to="/">
-                    <h2>Blog</h2>
-                </Link>
-            </div>    
-        </div>
-    )
-}
+  return (
+    <div className='navbar'>
+      <div className='container'>
+        <Link to='/'>
+          <h2>Blog</h2>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
