1dce3fbefdb6fdaf30bffdde75846ce5478ea2ee
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-27 11:56:21 +0000

    Commit

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,39 +1,44 @@
 import React, { useContext, useEffect } from 'react';
 import BlogContext from '../context/BlogContext';
-import Spinner from './Spinner'
-import PostCard from './PostCard'
+import Spinner from './Spinner';
+import PostCard from './PostCard';
 
 const PostList = () => {
-    const blogContext = useContext(BlogContext);
-    const { getPosts, blogPosts, loading } = blogContext;
+  const blogContext = useContext(BlogContext);
+  const { getPosts, blogPosts, loading } = blogContext;
 
-    useEffect(() => {
-        getPosts();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+  useEffect(() => {
+    getPosts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    //console.log(blogPosts);
+  //console.log(blogPosts);
 
-    if (loading) {
-        return <Spinner />;
-      } else {
-            return (
-                <div className="posts">
-                    <div className="container">
-                        <h2 className="title">Posts</h2>
-                        <div className="posts-grid-container">
-                            {blogPosts.map((post, i) => {
-                                return (
-                                    <PostCard key={i} title={post.title} image={post.image} author={post.author} date ={post.date} id={post.id} />
-                                )
-                            })
-                            }
-                        </div>
-                    </div>
-                    
-                </div>
-            )
-        } 
-}
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div className='posts'>
+        <div className='container'>
+          <h2 className='title'>Posts</h2>
+          <div className='posts-grid-container'>
+            {blogPosts.map((post, i) => {
+              return (
+                <PostCard
+                  key={i}
+                  title={post.title}
+                  image={post.image}
+                  author={post.author}
+                  date={post.date}
+                  id={post.id}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
 
-export default PostList
+export default PostList;
