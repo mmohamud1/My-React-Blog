004b772c6234eddb9b66fd0e7445efb12ee46586
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-27 11:56:14 +0000

    Commit

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const PostCard = ({ image, id, title, author, date}) => {
-
-    return (
-        <div className='card'>
-            <div className='card-image' style={{ backgroundImage: `url('${image}')` }}></div>
-            <div className='card-info'>
-                <div className='card-title'>
-                    <Link to={`/${id}`}>
-                        <p className="purple">{title}</p>
-                    </Link>
-                </div>
-                <div className='card-author-section'>
-                    <p>{author}</p>
-                    <p>{date}</p>
-                </div>
-            </div>
+const PostCard = ({ image, id, title, author, date }) => {
+  return (
+    <div className='card'>
+      <div
+        className='card-image'
+        style={{ backgroundImage: `url('${image}')` }}
+      ></div>
+      <div className='card-info'>
+        <div className='card-title'>
+          <Link to={`/${id}`}>
+            <p className='purple'>{title}</p>
+          </Link>
         </div>
-    )
-}
+        <div className='card-author-section'>
+          <p>{author}</p>
+          <p>{date}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default PostCard;
